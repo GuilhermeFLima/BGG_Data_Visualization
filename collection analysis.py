b02f25784095a99450a848d45b5188192b42421b
Guilherme Frederico Lima <gflogos@gmail.com>
2022-02-04 22:07:14 -0600

    Clean up.
    Coloring the bins didn't produce satisfactory results. Reverted back to old format.

--- collection analysis.py
+++ collection analysis.py
@@ -2,13 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-name = 'GFLima'
-file = 'GFLima_bgg_data.csv'
+name = 'Kara'
+file = 'howlsthunder_bgg_data.csv'
 # read csv file into pandas DataFrame
 bgg_data = pd.read_csv(file)
 
 # select relevant columns and drop duplicates
-bgg_data = bgg_data[['objectname', 'baverage', 'avgweight', 'rank', 'yearpublished', 'itemtype']].drop_duplicates()
+bgg_data = bgg_data[['objectname', 'baverage', 'avgweight', 'yearpublished', 'itemtype']].drop_duplicates()
 
 # remove expansions
 bgg_data_sa = bgg_data[bgg_data['itemtype'] == 'standalone']
@@ -25,8 +25,7 @@ data_max = bgg_data_sa_recent_yp.max()
 data_min = bgg_data_sa_recent_yp.min()
 
 # Series histogram
-# bgg_data_sa_recent_yp.plot.hist(bins=range(data_min, data_max+2), align='mid', width=0.8)
-plt.hist(bgg_data_sa_recent_yp, bins=range(data_min, data_max+2), align='mid', width=0.8)
+h = plt.hist(bgg_data_sa_recent_yp, bins=range(data_min, data_max+2), align='mid', width=0.8)
 pos = np.arange(data_min, data_max+2) + 0.4
 yearslabels = np.arange(data_min, data_max+2)
 plt.xticks(pos, yearslabels, rotation=90, alpha=0.8)
@@ -35,4 +34,6 @@ plt.grid(visible=True, axis='y', linestyle='--')
 plt.suptitle(name, fontsize='x-large', ha='center', weight='bold')
 plt.title('Games by Publication Date, 1900 - 2022', ha='center')
 plt.show()
+
+
 
