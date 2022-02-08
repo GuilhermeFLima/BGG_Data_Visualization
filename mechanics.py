c79bd0822e0ca7d48a0df29314555c09e4b011b9
Guilherme Frederico Lima <gflogos@gmail.com>
2022-02-08 03:01:42 -0600

    Script to get full web address from game id.
    Finished the script that retrieves the web address for the main page
    of a game given its ID.

--- mechanics.py
+++ mechanics.py
@@ -3,31 +3,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import requests
 from selenium import webdriver
 import re
 
-pattern = r'(?:href=\"/boardgamemechanic.*?)>([\w /-]+?)<'
-regex = re.compile(pattern)
+# first we need to get the actual page title since the name of the boardgame and
+# the name on the webpage can be different:
+# eg: "A Fake Artist Goes to New York" and "https://boardgamegeek.com/boardgame/135779/fake-artist-goes-new-york"
 
-url = "https://boardgamegeek.com/boardgame/31260/agricola/credits"
-driver = webdriver.Firefox()
-driver.get(url)
-source_text = driver.page_source
-print(regex.findall(source_text))
-driver.quit()
-
-url2 = "https://boardgamegeek.com/boardgame/342942/ark-nova/credits"
-driver = webdriver.Firefox()
-driver.get(url2)
-source_text = driver.page_source
-print(regex.findall(source_text))
-driver.quit()
+game_id = "135779"
+main_page_url = "https://boardgamegeek.com/boardgame/" + game_id
+main_page = requests.get(main_page_url)
+main_page_text = main_page.text
+#print(main_page_text)
+address_pattern = '(https://boardgamegeek.com/boardgame/[\d]+/[\w-]+)'
+address_regex = re.compile(address_pattern)
+address_find = address_regex.search(main_page_text)
+main_address = main_page_text[address_find.start():address_find.end()]
+print(main_address)
 
 
-# second pattern to find the names of the mechanics:
-#pattern2 = r'(?:\"name\"):\"([\w ]+)\"'
-# pattern2 = r'<td>([\w ]+)<span id='
-# regex2 = re.compile(pattern2)
-# print(regex2.findall(mechanics))
-# print(html.find("\"boardgamemechanic\""))
-#print(html)
+# pattern = r'(?:href=\"/boardgamemechanic.*?)>([\w /-]+?)<'
+# regex = re.compile(pattern)
+#
+# url = "https://boardgamegeek.com/boardgame/31260/agricola/credits"
+# driver = webdriver.Firefox()
+# driver.get(url)
+# source_text = driver.page_source
+# print(regex.findall(source_text))
+# driver.quit()
+#
+# url2 = "https://boardgamegeek.com/boardgame/342942/ark-nova/credits"
+# driver = webdriver.Firefox()
+# driver.get(url2)
+# source_text = driver.page_source
+# print(regex.findall(source_text))
+# driver.quit()
