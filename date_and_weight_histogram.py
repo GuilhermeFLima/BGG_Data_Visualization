f18777b27a591a9f2aea232b5ebcb0ed56808739
Guilherme Frederico Lima <gflogos@gmail.com>
2022-02-17 22:34:22 -0600

    data_clean()

--- date_and_weight_histogram.py
+++ date_and_weight_histogram.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # In order to use this script on your own files, you must alter the
-# two following variables:
+# following two variables:
 #
 # 1. The user's name to be featured on the plot.
 name = "Test"
@@ -21,18 +21,23 @@ file = "test_bgg_data.csv"
 # read csv file into pandas DataFrame
 bgg_data = pd.read_csv(file)
 
-# select relevant columns and drop duplicates
-bgg_data = bgg_data[['objectname', 'baverage', 'avgweight', 'yearpublished', 'itemtype']].drop_duplicates()
 
-# remove expansions
-bgg_data_sa = bgg_data[bgg_data['itemtype'] == 'standalone']
-# removing old games (before 1900)
-bgg_data_sa_recent = bgg_data_sa[bgg_data_sa['yearpublished'] >= 1900]
+def data_clean(dataframe):
+    # select relevant columns and drop duplicates
+    df = dataframe[['objectname', 'baverage', 'avgweight', 'yearpublished', 'itemtype']].drop_duplicates()
+    # remove expansions
+    df = df[df['itemtype'] == 'standalone']
+    # removing old games (before 1900)
+    df = df[df['yearpublished'] >= 1900]
+    # add weight class column
+    df['weightclass'] = df['avgweight'].map(np.floor)
+    return df
 
-# add weight class column
-bgg_data_sa_recent['weightclass'] = bgg_data_sa_recent['avgweight'].map(np.floor)
+
+bgg_data = data_clean(bgg_data)
+
 # grouping by weightclass
-data_grouped = bgg_data_sa_recent['yearpublished'].groupby(bgg_data_sa_recent['weightclass'])
+data_grouped = bgg_data['yearpublished'].groupby(bgg_data['weightclass'])
 # extracting just the dataframes from groupby object
 data_frames = [df.astype('object') for group, df in data_grouped]
 weightgroups = [group for group, df in data_grouped]
@@ -44,31 +49,24 @@ weightnames = dict(zip([0.0, 1.0, 2.0, 3.0, 4.0],
 weightgroupsnamed = [weightnames[group] for group in weightgroups]
 
 # pandas Series for publication years
-bgg_data_sa_recent_yp = bgg_data_sa_recent['yearpublished']
+years = bgg_data['yearpublished']
 
 # create matplotlib figure
 plt.figure(figsize=(20, 8), dpi=80)
 
-data_max = bgg_data_sa_recent_yp.max()
-data_min = bgg_data_sa_recent_yp.min()
+data_max = years.max()
+data_min = years.min()
 
-# colors blue to red
-colors = [(x/5, 0, 1 - x/5) for x in range(5)]
-# colors green and orange from bgg website for weights
-green = np.array((62/255, 214/255, 144/255))
-orange = np.array((255./255, 97./255, 48./255))
-# linear interpolation for intermediary colors
-bggcolors_linear = [green*(1 - t) + orange*t for t in np.arange(0, 1.25, 0.25)]
 # manual choice of colors
-bggcolors2 = np.array([(82/255, 255/255, 229/255),
+mycolors = np.array([(82/255, 255/255, 229/255),
                        (95/255, 214/255, 77/255),
                        (255/255, 231/255, 74/255),
                        (255/255, 162/255, 57/255),
                        (255./255, 97./255, 48./255)])
 # divide to add hue
-bggcolors2 = bggcolors2/(1.1, 1.1, 1.1)
+mycolors = mycolors/(1.1, 1.1, 1.1)
 colorofweights = dict(zip(['light', 'light - medium', 'medium', 'medium - heavy', 'heavy'],
-                          bggcolors2))
+                          mycolors))
 colors = [colorofweights[groupname] for groupname in weightgroupsnamed]
 # Series histogram
 plt.hist(data_frames, bins=range(data_min, data_max+2), align='mid', width=0.8, stacked=True, color=colors, alpha=1.0)
